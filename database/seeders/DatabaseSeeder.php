70a7257c88b8ea1c38e0213b9721baecf767443f
Vahn <vahnmarty@gmail.com>
2023-02-16 03:14:29 +0800

    added seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call(RolesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
         $this->call(PropertiesTableSeeder::class);
+        $this->call(ServicesTableSeeder::class);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
