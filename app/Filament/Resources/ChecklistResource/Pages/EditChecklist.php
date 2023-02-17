490bda9d9d904aaea42db96ff4ba9b4e6d98bce5
Vahn <vahnmarty@gmail.com>
2023-02-18 07:33:23 +0800

    checklists on edit page

--- app/Filament/Resources/ChecklistResource/Pages/EditChecklist.php
+++ app/Filament/Resources/ChecklistResource/Pages/EditChecklist.php
@@ -4,8 +4,10 @@ namespace App\Filament\Resources\ChecklistResource\Pages;
 
 use Filament\Forms;
 use App\Models\Service;
+use App\Models\Checklist;
 use Filament\Pages\Actions;
 use Filament\Pages\Actions\Action;
+use Filament\Forms\ComponentContainer;
 use Filament\Resources\Pages\EditRecord;
 use Illuminate\Database\Eloquent\Collection;
 use App\Filament\Resources\ChecklistResource;
@@ -19,16 +21,31 @@ class EditChecklist extends EditRecord
         return [
             Actions\DeleteAction::make(),
             // Action::make('checklists')
-            //     ->action(function (Collection $records, array $data): void {
-            //         //return redirect('');
+            //     ->icon('heroicon-s-cog')
+            //     ->color('secondary')
+            //     ->mountUsing(fn (Forms\ComponentContainer $form, Checklist $record) => $form->fill([
+            //         'services' => []
+            //     ]))
+            //     ->action(function (Checklist $record, array $data): void {
+            //         // Leave blank, Automatic;
             //     })
             //     ->form([
             //         Forms\Components\CheckboxList::make('services')
-            //             ->label('Services')
             //             ->relationship('services', 'name')
-            //             ->options(Service::orderBy('name')->get()->pluck('name', 'id'))
             //             ->required(),
             //     ])
+            Action::make('checklists')
+                ->action(function (Collection $records, array $data) {
+                    return redirect(request()->header('Referer'));
+                })
+                ->mountUsing(fn (Forms\ComponentContainer $form) => $form->fill([]))
+                ->form([
+                    Forms\Components\CheckboxList::make('services')
+                        ->label('Services')
+                        ->relationship('services', 'name')
+                        ->options(Service::orderBy('name')->get()->pluck('name', 'id'))
+                        ->required(),
+                ])
         ];
     }
 }
